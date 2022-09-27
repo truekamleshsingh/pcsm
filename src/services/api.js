81e05988218032bbb72ccb76c9107420685a63af
Kamlesh Singh <tksguide8@gmail.com>
2022-09-27 13:40:07 +0530

    Make better with server v1.0.1

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const URL = 'https://pcsmapi.herokuapp.com/'
+const URL = 'https://pcsmapi.herokuapp.com'
 // const URL = 'http://localhost:3001'
 
 export const addSubcriber = async (subscriberData) => {
